b4210624e0a7ac6e188546eeaf70dcf6b59108a2
Matt Harrison <hi@matt-harrison.com>
2023-04-13 08:34:13 +0100

    add flag

---
+++ packages/serverless-user-feedback/functions.js
@@ -0,0 +1,19 @@
+'use strict';
+const AWS = require('aws-sdk');
+
+module.exports.flag = async (event) => {
+  const body = JSON.parse(Buffer.from(event.body, 'base64').toString());
+  const dynamoDb = new AWS.DynamoDB.DocumentClient();
+  const putParams = {
+    TableName: process.env.DYNAMODB_FLAG_TABLE,
+    Item: {
+      primary_key: body.name,
+      email: body.email,
+    },
+  };
+  await dynamoDb.put(putParams).promise();
+
+  return {
+    statusCode: 201,
+  };
+};
