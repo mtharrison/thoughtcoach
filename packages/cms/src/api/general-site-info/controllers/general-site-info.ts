4bdb9b7b316ccfe232605b36544c7a3f7dec9eeb
Matt Harrison <hi@matt-harrison.com>
2023-04-13 01:38:51 +0100

    add cms

---
+++ packages/cms/src/api/general-site-info/controllers/general-site-info.ts
@@ -0,0 +1,7 @@
+/**
+ * general-site-info controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::general-site-info.general-site-info');
---
+++ packages/cms/src/api/general-site-info/routes/general-site-info.ts
@@ -0,0 +1,7 @@
+/**
+ * general-site-info router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::general-site-info.general-site-info');
