8e21dfb671bf182bf551a430339c51d9e9537a6d
Matt Harrison <hi@matt-harrison.com>
2023-04-15 16:32:02 +0100

    add report modal

--- packages/frontend/src/components/distortions.tsx
+++ packages/frontend/src/components/distortions.tsx
@@ -8,9 +8,17 @@ import {
   GridItem,
   CardBody,
   CardFooter,
+  Modal,
+  ModalContent,
+  ModalCloseButton,
+  ModalBody,
+  ModalHeader,
+  ModalFooter,
+  ModalOverlay,
   Divider,
   Heading,
   Tooltip,
+  Textarea,
   Mark,
   Link,
   Text,
@@ -18,11 +26,16 @@ import {
 
 import { QuestionIcon } from '@chakra-ui/icons';
 
+import { useState } from 'react';
+
 import { DistortionProps, DistortionsProps } from '@/types';
 
 export function Distortions(
   props: DistortionsProps & { thought?: string; event?: string }
 ) {
+  const [feedbackModalOpen, setFeedbackModalOpen] = useState(false);
+  const [feedbackComment, setFeedbackComment] = useState('');
+
   const spans: string[] = [];
   const spanLookup: { [key: string]: { color: string; name: string }[] } = {};
 
@@ -53,58 +66,105 @@ export function Distortions(
   };
 
   return (
-    <Stack spacing={2}>
-      <Grid templateColumns="repeat(4, 1fr)" gap={4}>
-        <GridItem rowSpan={1} colSpan={{ sm: 4, md: 3 }}>
-          <Heading size={'md'} lineHeight="10" borderTopRadius="lg" mb={5}>
-            {chunks.map(({ match, text }, i) => {
-              if (!match) return text;
-              return (
-                <Tooltip
-                  key={i}
-                  rounded={'md'}
-                  label={spanLookup[text].map((m) => m.name).join(', ')}
-                  placement="top"
-                >
-                  <Mark
-                    whiteSpace={'break-spaces'}
-                    rounded={'md'}
-                    bg={bgBuilder(spanLookup[text].map((m) => m.color))}
-                    color="white"
-                    fontWeight={'bold'}
-                    px="1"
-                    py="1"
-                  >
-                    <span style={{ mixBlendMode: 'plus-lighter' }}>{text}</span>
-                  </Mark>
-                </Tooltip>
-              );
-            })}
-          </Heading>
-        </GridItem>
-        <GridItem rowSpan={1} colSpan={{ sm: 4, md: 1 }}>
-          <Stack alignItems={'flex-end'}>
-            <Button
-              leftIcon={<QuestionIcon />}
-              colorScheme="orange"
-              variant="solid"
-              maxW={'220px'}
-              mb={5}
-            >
-              Was this unhelpful?
-            </Button>
-          </Stack>
-        </GridItem>
-      </Grid>
-      <SimpleGrid
-        minChildWidth={{ sm: 'fit-content', md: '300px' }}
-        spacing="15px"
+    <>
+      <Modal
+        isCentered={true}
+        isOpen={feedbackModalOpen}
+        onClose={() => setFeedbackModalOpen(false)}
+        size={'2xl'}
       >
-        {props.distortions.map((distortion: DistortionProps, i) => {
-          return <Distortion key={i} {...distortion} />;
-        })}
-      </SimpleGrid>
-    </Stack>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Provide feedback to us</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <Text>
+              We're sorry this reponse was not helpful to you. We're commited to
+              listening to user feedback and improving the application to give
+              the best help to our users.
+            </Text>
+            <Text mt={5}>
+              You can report this to us (with an optional comment) and we'll
+              look into it.
+            </Text>
+            <Text fontWeight={'bold'} mt={5}>
+              By reporting we will be able to see both your input and the
+              provided response, but this will be anonymous.
+            </Text>
+            <Textarea
+              mt={5}
+              value={feedbackComment}
+              onChange={(ev) => setFeedbackComment(ev.target.value)}
+              placeholder="Help us to understand where we went wrong (optional)"
+            ></Textarea>
+          </ModalBody>
+
+          <ModalFooter>
+            <Button colorScheme="blue" mr={3}>
+              Submit feedback
+            </Button>
+            <Button onClick={() => setFeedbackModalOpen(false)} variant="ghost">
+              Cancel
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+      <Stack spacing={2}>
+        <Grid templateColumns="repeat(4, 1fr)" gap={4}>
+          <GridItem rowSpan={1} colSpan={{ sm: 4, md: 3 }}>
+            <Heading size={'md'} lineHeight="10" borderTopRadius="lg" mb={5}>
+              {chunks.map(({ match, text }, i) => {
+                if (!match) return text;
+                return (
+                  <Tooltip
+                    key={i}
+                    rounded={'md'}
+                    label={spanLookup[text].map((m) => m.name).join(', ')}
+                    placement="top"
+                  >
+                    <Mark
+                      whiteSpace={'break-spaces'}
+                      rounded={'md'}
+                      bg={bgBuilder(spanLookup[text].map((m) => m.color))}
+                      color="white"
+                      fontWeight={'bold'}
+                      px="1"
+                      py="1"
+                    >
+                      <span style={{ mixBlendMode: 'plus-lighter' }}>
+                        {text}
+                      </span>
+                    </Mark>
+                  </Tooltip>
+                );
+              })}
+            </Heading>
+          </GridItem>
+          <GridItem rowSpan={1} colSpan={{ sm: 4, md: 1 }}>
+            <Stack alignItems={'flex-end'}>
+              <Button
+                leftIcon={<QuestionIcon />}
+                colorScheme="orange"
+                variant="solid"
+                onClick={() => setFeedbackModalOpen(true)}
+                maxW={'220px'}
+                mb={5}
+              >
+                Was this unhelpful?
+              </Button>
+            </Stack>
+          </GridItem>
+        </Grid>
+        <SimpleGrid
+          minChildWidth={{ sm: 'fit-content', md: '300px' }}
+          spacing="15px"
+        >
+          {props.distortions.map((distortion: DistortionProps, i) => {
+            return <Distortion key={i} {...distortion} />;
+          })}
+        </SimpleGrid>
+      </Stack>
+    </>
   );
 }
 
