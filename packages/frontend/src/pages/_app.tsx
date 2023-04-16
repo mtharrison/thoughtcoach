4636677e581349e5f46a48b000458a27d76b6f4e
Matt Harrison <hi@matt-harrison.com>
2023-04-16 17:53:38 +0100

    rm theme from pages

--- packages/frontend/src/pages/_app.tsx
+++ packages/frontend/src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import '../styles/globals.css';
 import { ChakraProvider } from '@chakra-ui/react';
-import theme from './_theme';
+import theme from '../_theme';
 import { useEffect } from 'react';
 
 function App({ Component, pageProps }: { Component: any; pageProps: any }) {
